a32f1622580f60ea3b7e664c916a98847d192d97
Trai Lynne <trailynne@gmail.com>
2019-01-22 15:39:27 -0500

    POST `/api/zoos` async/await used checks for name success => 201 with new id incomplete => 400 with error message internal error => 500 with error message

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile');
 
 const server = express();
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+// C - Create
+server.post('/api/zoos', async (req, res) => {
+  const newRecord = req.body;
+  
+  try {
+    if (!newRecord.name || newRecord.name === '') {
+      res
+        .status(400)
+        .json({
+          errorMessage: 'Please provide a name for the new record'
+        });
+    } else {
+      let newId = await db.insert(newRecord).into('zoos');
+      
+      newId ?
+        res
+          .status(201)
+          .json(newId[0])
+      :
+        res
+          .status(500)
+          .json({
+          errorMessage: 'Houston we have a problem'
+        });
+      
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        errorMessage: 'Houston we have a problem'
+      });
+  }
+});
 
 const port = 3300;
 server.listen(port, function() {
